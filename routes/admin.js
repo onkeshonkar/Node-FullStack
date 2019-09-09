e27cd3563ef83b686ae63cec118d75becf66d5b5
Onkeshonkar <onkeshkumaronkar315@gmail.com>
2019-09-09 22:45:09 +0530

    MVC concept implemented.

--- routes/admin.js
+++ routes/admin.js
@@ -1,21 +1,11 @@
-const path = require('path');
-
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsControllers = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
+router.get('/add-product', productsControllers.getAddProduct);
 
-router.get('/add-product', (req, res, next) => {
-    res.render('add-product', { pageTitle: 'Add Product',path: '/admin/add-product', formCSS: true, productCSS: true, activeAddProduct: true });
-});
+router.post('/add-product', productsControllers.postAddProduct);
 
-router.post('/add-product', (req, res, next) => {
-    products.push({ title: req.body.title })
-    res.redirect('/');
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
